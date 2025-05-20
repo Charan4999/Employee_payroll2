3b4c54e448aee1c30105fd3be7a2684609c8101f
Charan <veerankicharan4@gmail.com>
2025-05-20 12:23:31 +0530

    Welcome

---
+++ src/main/java/com/example/intro/EmployeePayroll2Application.java
@@ -0,0 +1,13 @@
+package com.example.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayroll2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayroll2Application.class, args);
+	}
+
+}
